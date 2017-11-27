300a87e88b67c3b288163f6bd0c5f6f4aeb70c93
Stephen <stephen.pamphile001@mymdc.net>
2017-11-26 21:44:05 -0500

    Initial commit

--- COP4656MusicApp/COP4656MusicApp.Android/MainActivity.cs
+++ COP4656MusicApp/COP4656MusicApp.Android/MainActivity.cs
@@ -6,13 +6,14 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Media;
 
 namespace COP4656MusicApp.Droid
 {
 	[Activity (Label = "COP4656MusicApp.Android", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+        
 
 		protected override void OnCreate (Bundle bundle)
 		{
@@ -21,13 +22,19 @@ namespace COP4656MusicApp.Droid
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+            Songs songs = new Songs();
+            var listView = FindViewById<ListView>(Resource.Id.songsList);
+            var adapter = new ArrayAdapter(this, Resource.Layout.CustomListView, Android.Resource.Id.Text1, songs.GetSongs());
+
+            listView.Adapter = adapter;
+
+            listView.ItemClick += (sender, e) =>
+            {
+                var songActivity = new Intent(this, typeof(SongActivity));
+                songActivity.PutExtra("songTitle", songs.GetSong(e.Position));
+                songActivity.PutExtra("songIndex", e.Position);
+                StartActivity(songActivity);
+            };
 		}
 	}
 }
--- COP4656MusicApp/COP4656MusicApp.Android/Resources/Resource.designer.cs
+++ COP4656MusicApp/COP4656MusicApp.Android/Resources/Resource.designer.cs
@@ -45,7 +45,28 @@ namespace COP4656MusicApp.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int acousticbreeze = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int back = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int Icon = 2130837506;
+			
+			// aapt resource value: 0x7f020003
+			public const int img_174171 = 2130837507;
+			
+			// aapt resource value: 0x7f020004
+			public const int pause_button_png_image_80706 = 2130837508;
+			
+			// aapt resource value: 0x7f020005
+			public const int play_button = 2130837509;
+			
+			// aapt resource value: 0x7f020006
+			public const int prev_song = 2130837510;
+			
+			// aapt resource value: 0x7f020007
+			public const int stop = 2130837511;
 			
 			static Drawable()
 			{
@@ -60,8 +81,35 @@ namespace COP4656MusicApp.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050000
-			public const int myButton = 2131034112;
+			// aapt resource value: 0x7f060003
+			public const int artwork = 2131099651;
+			
+			// aapt resource value: 0x7f060002
+			public const int back = 2131099650;
+			
+			// aapt resource value: 0x7f060000
+			public const int linearLayout1 = 2131099648;
+			
+			// aapt resource value: 0x7f060007
+			public const int nextSong = 2131099655;
+			
+			// aapt resource value: 0x7f060009
+			public const int pause = 2131099657;
+			
+			// aapt resource value: 0x7f060006
+			public const int play = 2131099654;
+			
+			// aapt resource value: 0x7f060005
+			public const int prevSong = 2131099653;
+			
+			// aapt resource value: 0x7f060004
+			public const int songTitle = 2131099652;
+			
+			// aapt resource value: 0x7f060001
+			public const int songsList = 2131099649;
+			
+			// aapt resource value: 0x7f060008
+			public const int stop = 2131099656;
 			
 			static Id()
 			{
@@ -77,7 +125,13 @@ namespace COP4656MusicApp.Droid
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int CustomListView = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
+			
+			// aapt resource value: 0x7f030002
+			public const int Song = 2130903042;
 			
 			static Layout()
 			{
@@ -89,14 +143,36 @@ namespace COP4656MusicApp.Droid
 			}
 		}
 		
+		public partial class Raw
+		{
+			
+			// aapt resource value: 0x7f040000
+			public const int bensound_acousticbreeze = 2130968576;
+			
+			// aapt resource value: 0x7f040001
+			public const int bensound_happiness = 2130968577;
+			
+			// aapt resource value: 0x7f040002
+			public const int bensound_sweet = 2130968578;
+			
+			static Raw()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Raw()
+			{
+			}
+		}
+		
 		public partial class String
 		{
 			
-			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			// aapt resource value: 0x7f050001
+			public const int app_name = 2131034113;
 			
-			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
+			// aapt resource value: 0x7f050000
+			public const int hello = 2131034112;
 			
 			static String()
 			{
